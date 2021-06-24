4e412da1cd65a638cfa1587ae6f0b57149161a54
tabcsnag <tabcsnag@TABM5524.local>
2021-06-24 15:40:30 +0200

    Update debugbar_1624372402.json, debugbar_1624372408.json, and 4 more files...

---
+++ writable/smarty/templates_c/81baef0640699d9685f26349a67bfea8d464f1c3_0.file.mainTemplate.tpl.php
@@ -0,0 +1,123 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-06-22 09:33:22
+  from 'C:\Users\tabcsnag\Dropbox\Private\web\munkak\ci-TodoApp\app\Views\mainTemplate.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_60d1f4b2b259a4_95857558',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '81baef0640699d9685f26349a67bfea8d464f1c3' => 
+    array (
+      0 => 'C:\\Users\\tabcsnag\\Dropbox\\Private\\web\\munkak\\ci-TodoApp\\app\\Views\\mainTemplate.tpl',
+      1 => 1624372298,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_60d1f4b2b259a4_95857558 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css">
+
+        <!-- sajat stiluslap -->
+        <link rel = "stylesheet" type = "text/css" href = "css/style.css">
+
+        <title>Hello, world!</title>
+    </head>
+    <body class="bg-light">
+        <div class="mainContainer">
+            <!-- Menü -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a class="navbar-brand" href="index.php">
+                    <img src="images/logo.png" width="30" alt="Logo"> TodoApp</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Kezdőlap<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+                        <section class="BodyMain">
+                <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_38845076160d1f4b2b1aaa2_60193582', 'BodyMain');
+?>
+
+            </section>
+
+                        <section class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-center">Copyright© 2021</div>
+                        <div class="col text-center">by Nagy Csaba e.v.</div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+  
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <?php echo '<script'; ?>
+ src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+
+        <?php echo '<script'; ?>
+ src="scripts/myscripts.js"><?php echo '</script'; ?>
+>
+
+    </body>
+</html><?php }
+/* {block 'BodyMain'} */
+class Block_38845076160d1f4b2b1aaa2_60193582 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'BodyMain' => 
+  array (
+    0 => 'Block_38845076160d1f4b2b1aaa2_60193582',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+                    <h1 class="text-uppercase">Hello, world!</h1>
+                <?php
+}
+}
+/* {/block 'BodyMain'} */
+}
